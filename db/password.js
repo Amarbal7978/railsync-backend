ba4b10eb9c0414fa0666310684c50ef4bfd6ac86
Amarbal7978 <bal985258@gmail.com>
2026-09-18 12:21:00 +0530

    Fix password helper exports

--- db/password.js
+++ db/password.js
@@ -1,9 +1,14 @@
 const bcrypt = require("bcryptjs");
 
+async function hashPassword(password) {
+  return bcrypt.hash(password, 10);
+}
+
 async function verifyPassword(password, storedPassword) {
   return bcrypt.compare(password, storedPassword);
 }
 
 module.exports = {
+  hashPassword,
   verifyPassword,
 };
